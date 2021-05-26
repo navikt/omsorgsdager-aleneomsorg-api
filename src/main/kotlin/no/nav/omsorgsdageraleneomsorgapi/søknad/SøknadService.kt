4fc708e7078e9d5d8898c88a49d21712b013b31d
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-05-26 15:23:16 +0200

    Refaktorerer registrering av søknad.

--- src/main/kotlin/no/nav/omsorgsdageraleneomsorgapi/søknad/SøknadService.kt
+++ src/main/kotlin/no/nav/omsorgsdageraleneomsorgapi/søknad/SøknadService.kt
@@ -34,30 +34,35 @@ class SøknadService(
         søknad.valider()
         LOGGER.info(formaterStatuslogging(søknad.søknadId, "validert OK"))
 
-        if (søknad.barn.size > 1) {
-            val søknader = søknad.splittTilSøknadPerBarn()
-            LOGGER.info("SøknadId:${søknad.søknadId} splittet ut til ${søknader.map { it.søknadId }}")
+        if (søknad.barn.size > 1) registrerFlereSøknader(søknad, metadata, søker)
+        else registrerSøknad(søknad, metadata, søker)
+    }
 
-            søknader.forEach { it.tilK9Format(søker).valider() }
-            LOGGER.info(formaterStatuslogging(søknad.søknadId, "K9Format validert OK"))
+    private fun registrerFlereSøknader(søknad: Søknad, metadata: Metadata, søker: Søker) {
+        val søknader = søknad.splittTilSøknadPerBarn()
+        LOGGER.info("SøknadId:${søknad.søknadId} splittet ut til ${søknader.map { it.søknadId }}")
 
-            try {
-                kafkaProdusent.beginTransaction()
-                søknader.forEach { kafkaProdusent.produserKafkamelding(søknad = it.tilKomplettSøknad(søker), metadata = metadata) }
-                kafkaProdusent.commitTransaction()
-            } catch (e: Exception) {
-                LOGGER.error("Feilet ved produsering av kafkamelding")
-                kafkaProdusent.abortTransaction()
-                throw e
-            }
-        } else {
-            søknad.tilK9Format(søker).valider()
-            LOGGER.info(formaterStatuslogging(søknad.søknadId, "K9Format validert OK"))
+        søknader.forEach { it.tilK9Format(søker).valider() }
+        LOGGER.info(formaterStatuslogging(søknad.søknadId, "K9Format validert OK"))
 
+        try {
             kafkaProdusent.beginTransaction()
-            kafkaProdusent.produserKafkamelding(søknad = søknad.tilKomplettSøknad(søker), metadata = metadata)
+            søknader.forEach { kafkaProdusent.produserKafkamelding(søknad = it.tilKomplettSøknad(søker), metadata = metadata) }
             kafkaProdusent.commitTransaction()
+        } catch (e: Exception) {
+            LOGGER.error("Feilet ved produsering av kafkamelding")
+            kafkaProdusent.abortTransaction()
+            throw e
         }
+    }
+
+    private fun registrerSøknad(søknad: Søknad, metadata: Metadata, søker: Søker) {
+        søknad.tilK9Format(søker).valider()
+        LOGGER.info(formaterStatuslogging(søknad.søknadId, "K9Format validert OK"))
+
+        kafkaProdusent.beginTransaction()
+        kafkaProdusent.produserKafkamelding(søknad = søknad.tilKomplettSøknad(søker), metadata = metadata)
+        kafkaProdusent.commitTransaction()
     }
 
 }
